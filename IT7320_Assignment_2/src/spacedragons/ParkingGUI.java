d3850bffe669427313b63c1e368131cb1bead805
Anthony <Anthony@156.59.130.162>
2018-09-12 11:26:22 +1200

    Updated text boxes so they can't be user edited

--- IT7320_Assignment_2/src/spacedragons/ParkingGUI.java
+++ IT7320_Assignment_2/src/spacedragons/ParkingGUI.java
@@ -74,6 +74,9 @@ public class ParkingGUI extends JFrame {
 	JButton btnStop = new JButton("Stop Parking");
 	int dragonFined; 
 	JTextArea additionalChargesTextArea = new JTextArea();
+	
+	
+	
 
 
 	public static void main(String[] args) {
@@ -123,18 +126,21 @@ public class ParkingGUI extends JFrame {
 		
 		JTextPane timeText = new JTextPane();
 		timeText.setText("0");
+		timeText.setEditable(false);
 		timeText.setBorder(BorderFactory.createLineBorder(Color.black));
 		timeText.setBounds(9, 131, 115, 20);
 		contentPane.add(timeText);
 		
 		JTextPane moneyOwed = new JTextPane();
 		moneyOwed.setText("$0");
+		moneyOwed.setEditable(false);
 		moneyOwed.setBorder(BorderFactory.createLineBorder(Color.black));
 		moneyOwed.setBounds(152, 131, 123, 20);
 		contentPane.add(moneyOwed);
 		
 		JTextPane balanceTextPane = new JTextPane();
 		balanceTextPane.setText("$ " + Integer.toString(balance));
+		balanceTextPane.setEditable(false);
 		balanceTextPane.setBorder(BorderFactory.createLineBorder(Color.black));
 		balanceTextPane.setBounds(152, 42, 123, 20);
 		contentPane.add(balanceTextPane);
@@ -244,6 +250,7 @@ public class ParkingGUI extends JFrame {
 		additionalChargesTextArea.setBounds(285, 42, 164, 184);
 		//contentPane.add(additionalChargesTextArea);
 		additionalChargesTextArea.setBorder(BorderFactory.createLineBorder(Color.black));
+		additionalChargesTextArea.setEditable(false);
 		scroll.setBounds(302, 42, 147, 184);
 		scroll.setViewportView(additionalChargesTextArea);
 		contentPane.add(scroll);
